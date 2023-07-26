a5530393c8400ebc76bbbb1e4ed455d520ef6a11
QuocAnh <qanhanh06@gmail.com>
2023-07-27 02:31:56 +0700

    buoi16

---
+++ js/buoi16/js/script.js
@@ -0,0 +1,55 @@
+// Bai 1
+var a = 5;
+var b = 6;
+var a = b + a;
+var b = a - b;
+var a = a - b;
+console.log(a,b);
+// bai2
+s = 10 + 20 + 5**10 / 2 ;
+console.log(s);
+// bai3
+a = 5;
+b = 8;
+c = 9;
+if(a > b && a > c){
+    console.log("so lon nhat la: ", a);
+}
+else if(b > c && b > a){
+    console.log("so lon nhat la: ", b);
+}
+else{
+    console.log("so lon nhat la: ", c);
+    
+}
+// bai 4
+a = -4;
+b = 5;
+if(a * b < 0){
+    console.log("Khac dau");
+}
+else
+{
+    console.log("Cung dau");
+}
+//  Bai 5
+var a = 6;
+var b = 4; 
+var c = 5 ;
+var tg=0;
+if(a < b ){
+   tg = a
+   a = b
+   b = tg 
+} 
+else if(a < c ){
+    tg = a
+   a = c 
+   c = tg 
+}
+else if (b < c){
+    tg = b
+    b = c
+    c = tg
+}
+console.log(c,b,a);
---
+++ js/buoi16/note.md
@@ -0,0 +1,17 @@
+## Nguyên tắc đặt tên
+
+## camelCase
+
+=> đặt tên biến , hàm
+Từ đầu viết thường
+Từ sau viết hoa chữ cái đầu
+
+## PascalCase
+
+=> Ký tự đầu của mỗi từ đều viết hoa
+đặt tên các class, các component
+
+## underscore
+
+viết thường nối với nhau bởi đấu gạch dưới
+=> Thường đăỵ trong các key của Api, tên trường trong data base , tên hằng số (Viết Hoa Hết)
