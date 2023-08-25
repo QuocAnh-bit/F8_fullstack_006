e6034f0e451312c7511e61fa802ed13076aa6e41
QuocAnh <qanhanh06@gmail.com>
2023-08-25 16:53:25 +0700

    bt24

--- buoi_17/buoi_17/js/ex01.js
+++ buoi_17/buoi_17/js/ex01.js
@@ -1,85 +1,79 @@
 // Bai 1 : Tính cước taxi
-var n = 121; //so km di duoc
-var numberKm_1 = 1,
-  numberKm_2 = 5,
-  numberKm_3 = 120; // cac nguong tinh tien
-var price_1 = 15000,
-  price_2 = 13500,
-  price_3 = 11000; // gia cac nguong
-var totalBill = 0;
-if (n <= numberKm_1) {
-  totalBill = n * price_1;
-} else if (n <= numberKm_2) {
-  totalBill = price_1 * numberKm_1 + (n - numberKm_1) * price_2;
-} else if (n > numberKm_2 && n <= numberKm_3) {
-  totalBill =
-    price_1 * numberKm_1 +
-    (numberKm_2 - numberKm_1) * price_2 +
-    (n - numberKm_2) * price_3;
-} else if (n > numberKm_3) {
-  totalBill =
-    (price_1 * numberKm_1 +
-      (numberKm_2 - numberKm_1) * price_2 +
-      (numberKm_3 - numberKm_2) * price_3 +
-      (n - numberKm_3) * price_3) *
-    0.9;
+var km = 121;
+var price1 = 15000; // Ngưỡng 1
+var price2 = 13500; // Ngưỡng 2
+var price3 = 11000; // Ngưỡng 3
+var discount = 10;
+var cost;
+if (km <= 1) {
+  cost = 1 * price1;
+} else {
+  if (km <= 5) {
+    cost = 1 * price1 + (km - 1) * price2;
+  } else {
+    cost = 1 * price1 + (5 - 1) * price2 + (km - 5) * price3;
+
+    if (km > 120) {
+      cost = (cost * (100 - discount)) / 100;
+    }
+  }
 }
-console.log(`Tổng tiền  phải thanh toán là: ${totalBill}đ`);
+console.log(cost);
 
 // Bai 2 : Tinh gia dien
-var n = 214; // so dien tieu thu
-var numberKwh_1 = 1678,
-  numberKwh_2 = 1734,
-  numberKwh_3 = 2014,
-  numberKwh_4 = 2536,
-  numberKwh_5 = 2834,
-  numberKwh_6 = 2927;
-var totalBill = 0;
-if (n > 0 && n <= 50) {
-  totalBill = n * numberKwh_1;
-} else if (n > 50 && n <= 100) {
-  totalBill = 50 * numberKwh_1 + (n - 50) * numberKwh_2;
-} else if (n > 100 && n <= 200) {
-  totalBill = 50 * numberKwh_1 + 50 * numberKwh_2 + (n - 100) * numberKwh_3;
-} else if (n > 200 && n <= 300) {
-  totalBill =
-    50 * numberKwh_1 +
-    100 * numberKwh_2 +
-    200 * numberKwh_3 +
-    (n - 350) * numberKwh_4;
-} else if (n > 300 && n <= 400) {
-  totalBill =
-    50 * numberKwh_1 +
-    100 * numberKwh_2 +
-    200 * numberKwh_3 +
-    300 * numberKwh_4 +
-    (n - 650) * numberKwh_5;
-} else if (n > 400) {
-  totalBill =
-    50 * numberKwh_1 +
-    100 * numberKwh_2 +
-    200 * numberKwh_3 +
-    300 * numberKwh_4 +
-    400 * numberKwh_5 +
-    (n - 1050) * numberKwh_6;
+var kwh = 100; // số điện
+var lv1 = 1678; // các ngưỡng
+var lv2 = 1734;
+var lv3 = 2014;
+var lv4 = 2536;
+var lv5 = 2834;
+var lv6 = 2927;
+var cost;
+if (kwh <= 0) {
+  console.log("Vui lòng nhập số điện khác 0");
+} else if (kwh <= 50) {
+  cost = kwh * lv1;
+} else if (kwh <= 100) {
+  cost = 50 * lv1 + (kwh - 50) * lv2;
+} else if (kwh <= 200) {
+  cost = 50 * lv1 + (100 - 50) * lv2 + (kwh - 100) * lv3;
+} else if (kwh <= 300) {
+  cost = 50 * lv1 + (100 - 50) * lv2 + (200 - 100) * lv3 + (kwh - 200) * lv4;
+} else if (kwh <= 400) {
+  cost =
+    50 * lv1 +
+    (100 - 50) * lv2 +
+    (200 - 100) * lv3 +
+    (300 - 200) * lv4 +
+    (kwh - 300) * lv5;
+} else {
+  cost =
+    50 * lv1 +
+    (100 - 50) * lv2 +
+    (200 - 100) * lv3 +
+    (300 - 200) * lv4 +
+    (400 - 300) * lv5 +
+    (kwh - 400) * lv6;
 }
-console.log(`Tổng tiền điện phải thanh toán là: ${totalBill}đ`);
+console.log(cost);
+
 // bai 3 tinh bieu thuc
-var n = 5;
-var subTotal = 1,
-  total = 0;
+var n = 2;
+var total = 0;
 for (var i = 1; i <= n; i++) {
-  subTotal = i * (i + 1);
-  total += subTotal;
+  total += i * (i + 1);
 }
 console.log(total);
+
 // bai 4 viet ham so nguyen
-function prime(number) {
+function isPrime(number) {
   var n = number;
-  if (n < 2) {
+  if (n % 1 !== 0 || n <= 1) {
+    // Nếu n k là số nguyên và n <= 1 thì trả về false
     return false;
   } else {
     for (var i = 2; i < n; i++) {
+      // vòng lặp kiểm tra nếu  như có nhiều hơn 1 ước thì trả về false
       if (n % i === 0) {
         return false;
       }
@@ -87,15 +81,14 @@ function prime(number) {
   }
   return true;
 }
-var n = 0;
-if (prime(n) === true) {
+var n = 5;
+if (isPrime(n)) {
   console.log(`So ${n} la so nguyen to`);
-} else if (prime(n) === false) {
+} else {
   console.log(`So ${n} la  khong la so nguyen to`);
 }
 // bai 5
 document.write(`<h2> Bài 5: Tam giác số</h2>`);
-
 var n = 5; // so dong
 var dem = 1;
 for (var i = 1; i <= n; i++) {
@@ -114,17 +107,13 @@ var dem = 0; // so dong
 for (var i = 1; i <= n; i++) {
   for (var j = 1; j <= n; j++) {
     if (i % 2 === 0) {
-      if (j % 2 === 0) {
-        document.write(`<div class="white"></div>`); // Ô màu trắng
-      } else {
-        document.write(`<div class="black"></div>`); // Ô màu đen
-      }
+      document.write(
+        `<div ${j % 2 === 0 ? `class="white"` : `class="black"`} ></div>`
+      ); // Ô màu trắng
     } else {
-      if (j % 2 === 0) {
-        document.write(`<div class="black"></div>`);
-      } else {
-        document.write(`<div class="white"></div>`);
-      }
+      document.write(
+        `<div ${j % 2 === 0 ? `class="black"` : `class="white"`} ></div>`
+      );
     }
   }
   document.write(`</br>`);
---
+++ buoi_24/js/ex01.js
@@ -0,0 +1,66 @@
+var todoInput = document.querySelector(".todo-input");
+var todoForm = document.querySelector(".todo-form");
+var todoWrapper = document.querySelector(".wrapper");
+
+function addTodoList(valueInput) {
+  return `<div class="todo-item">
+  <p class="">${valueInput}</p>
+  <div class="icon-todo-item">
+    <i class="fa-solid fa-pen-to-square"></i>
+    <i class="fa-solid fa-trash"></i>
+  </div>
+</div>`;
+}
+// Add Item
+todoForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  var valueInput = todoInput.value;
+  if (valueInput.length !== 0) {
+    var addItem = addTodoList(valueInput);
+    todoWrapper.insertAdjacentHTML("beforeend", addItem);
+    todoInput.value = "";
+  }
+});
+
+// Item Completed
+todoWrapper.addEventListener("click", function (e) {
+  console.log(e.target.className);
+  if (e.target.className === "") {
+    e.target.classList.add("checked");
+  } else if (e.target.className == "checked") {
+    e.target.classList.remove("checked");
+  }
+});
+
+// Item Edit
+todoWrapper.addEventListener("click", function (e) {
+  var valueInput = document.querySelector(".todo-item p");
+  var inputEdit = document.querySelector(".todo-input-item");
+  // Form Edit
+  if (e.target.className === "fa-solid fa-pen-to-square") {
+    e.target.parentElement.parentElement.remove();
+    var html = ` <form class="todo-form">
+    <input
+      type="text"
+      class="todo-input-item"
+      placeholder="What is the task today?"
+      value="${valueInput.innerText}"
+    />
+    <button type="button" class="todo-btn-item">Add Task</button>
+  </form>`;
+    todoWrapper.insertAdjacentHTML("beforebegin", html);
+  }
+  // After Edit
+  if (e.target.className === "todo-btn-item") {
+    e.target.parentElement.remove();
+    var valueEdit = addTodoList(inputEdit.value);
+    todoWrapper.insertAdjacentHTML("beforeend", html);
+  }
+});
+
+// Delete item TodoList
+todoWrapper.addEventListener("click", function (e) {
+  if (e.target.className === "fa-solid fa-trash") {
+    e.target.parentElement.parentElement.remove();
+  }
+});
