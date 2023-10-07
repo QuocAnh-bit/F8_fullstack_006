5d46905aab73d892cefda58c00f2110bab65d578
QuocAnh <qanhanh06@gmail.com>
2023-10-07 19:09:12 +0700

    buoi_35

--- buoi_35/js/config.js
+++ buoi_35/js/config.js
@@ -1,32 +1,3 @@
-// const severApi = ` http://localhost:3000`;
-
-// const postUser = async (data) => {
-//   // chuyển cú pháp cho phù hợp với application/x-www-form-urlencoded
-//   const dataUrlEndcoded = new URLSearchParams(data).toString();
-
-//   const response = await fetch(`${severApi}/todos`, {
-//     method: "POST",
-//     headers: {
-//       //   "Content-Type": "application/json",
-//       "Content-Type": "application/x-www-form-urlencoded",
-//     },
-//     // body: JSON.stringify(data)
-//     body: dataUrlEndcoded,
-//   });
-//   console.log(response);
-// };
-
-// const getUser = async () => {
-//   const response = await fetch(`${severApi}/todos`);
-//   const todos = await response.json();
-//   // đọc response headers
-//   console.log(response.headers.get("Content-Type"));
-// };
-// getUser();
-// // postUser({
-// //   name: "User 7",
-// //   email: "user7@gmail.com",
-// // });
 export const config = {
-  SERVER_API: "http://localhost:3000",
+  SERVER_API: "https://35y4ks-8080.csb.app",
 };
---
+++ buoi_35/js/main.js
@@ -0,0 +1,101 @@
+import { client } from "./client.js";
+
+let pagination = 1;
+let limitEl = 5;
+
+const getPost = async (query = {}) => {
+  const queryString = new URLSearchParams(query).toString();
+  const { data } = await client.get(`/posts?${queryString}}`);
+  return data;
+};
+const main = document.querySelector("main");
+const container = document.querySelector(".container");
+
+// getPost({
+//   _page: pagination,
+//   _limit: limitEl,
+// });
+const renderPost = async () => {
+  const datas = await getPost({
+    _page: pagination,
+    _limit: limitEl,
+  });
+
+  datas.forEach((post) => {
+    console.log(post);
+    const sectionHtml = `
+    <section class="section-item">
+          <a href="#" class="category-post">${post.category}</a>
+          <h2 class="title-post">${post.title}</h2>
+          <a href="#" class="user">
+            <div class="avt-user">
+              <img src="${post.avt}" alt="" />
+            </div>
+            <span class="name-user">${post.nameUser}</span>
+          </a>
+
+          <hr />
+          <p class="introductory">${post.introductory}</p>
+          <figure class="img-post">
+            <img
+              src="${post.urlImgPost[0].urlImg}"
+              alt="${post.urlImgPost[0].caption}"
+            />
+            <figcaption>${post.urlImgPost[0].caption}</figcaption>
+          </figure>
+          <div class="content-post">
+            ${
+              post.contentPost.length > 0
+                ? post.contentPost
+                    .map((sentence) => `<p>${sentence.content}</p>`)
+                    .join("")
+                : ""
+            }
+          </div>
+          <figure class="img-post">
+            <img
+              src="${post.urlImgPost[1].urlImg}"
+              alt="${post.urlImgPost[1].urlImg}
+            "
+            />
+            <figcaption>${post.urlImgPost[1].caption}</figcaption>
+          </figure>
+          <hr />
+          <div class="hashtag">
+            ${post.hashtag.length > 0 ? `<p>Chủ đề:</p>` : ""}
+            ${
+              post.hashtag.length > 0
+                ? post.hashtag
+                    .map(
+                      (tagItem) => `<a class="hashtag-item">${tagItem.tag}</a>`
+                    )
+                    .join("")
+                : ""
+            }
+          </div>
+        </section>`;
+    container.innerHTML += sectionHtml;
+  });
+  main.append(container);
+};
+const renderLoading = () => {
+  const loader = document.querySelector(".loader-wrap");
+  loader.style.display = "flex";
+  setTimeout(() => {
+    loader.style.display = "none";
+  }, 1000);
+};
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY + window.innerHeight);
+  console.log(document.documentElement.scrollHeight);
+
+  if (
+    window.scrollY + window.innerHeight + 100 >=
+    document.documentElement.scrollHeight
+  ) {
+    renderPost();
+  }
+});
+renderLoading();
+renderPost();
